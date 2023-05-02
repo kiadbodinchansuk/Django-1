445cdbdafaa16c40968627754404d7cf29045893
kiadbodin <kiadbodin@hotmail.com>
2023-05-02 11:00:45 +0700

    update

--- helloworld/helloworld/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from datetime import datetime
-import datetime
-
-def Greeting(request):
-    current_datetime = datetime.datetime.now()  
-    context={"current_datetime":current_datetime}
-    return render(request,"index.html",{"context":context})
